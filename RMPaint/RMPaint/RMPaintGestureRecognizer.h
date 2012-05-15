aae4f66bdb7030f9443f4b5b9b47a6aeeb159b17
Hermes Pique <hermes.pique@robotmedia.net>
2012-05-15 10:27:08 +0200

    Multi-touch painting with gesture recognizer

---
+++ RMPaint/RMPaint/RMPaintGestureRecognizer.h
@@ -0,0 +1,13 @@
+//
+//  RMDragGestureRecognizer.h
+//  RMPaint
+//
+//  Created by Hermes Pique on 5/15/12.
+//  Copyright (c) 2012 Robot Media SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMDragGestureRecognizer : UIGestureRecognizer
+
+@end
---
+++ RMPaint/RMPaint/RMPaintGestureRecognizer.m
@@ -0,0 +1,13 @@
+//
+//  RMDragGestureRecognizer.m
+//  RMPaint
+//
+//  Created by Hermes Pique on 5/15/12.
+//  Copyright (c) 2012 Robot Media SL. All rights reserved.
+//
+
+#import "RMDragGestureRecognizer.h"
+
+@implementation RMDragGestureRecognizer
+
+@end
